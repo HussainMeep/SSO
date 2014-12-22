b3f81ffcb4b749e144ca7cd0c5c8c1f9fcbce8b7
hussainmeep <aamir.hussain@machineparty.com>
2014-12-22 17:51:37 +0530

    data base created

--- sso/settings.py
+++ sso/settings.py
@@ -84,7 +84,8 @@ ACCOUNT_LOGIN_REDIRECT_URL = '/'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/home/ubuntu/sso.db',
     }
 }
 
