6a8c7372b7e4000f1d7811ee10954fd8826f3810
hussainmeep <aamir.hussain@machineparty.com>
2014-12-22 17:33:34 +0530

    path added

--- sso/wsgi.py
+++ sso/wsgi.py
@@ -8,7 +8,13 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR), '..')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sso.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
